a4a9997e9ae0e6bf6b200f094bfaa1577b1e93e9
AramisLins <122841235+AramisLins@users.noreply.github.com>
2023-07-04 19:49:52 -0300

    Add files via upload

---
+++ dia 7 Gerador paleta de cores/src/js/script.js
@@ -0,0 +1,29 @@
+const generateButton = document.querySelector('#generate-button')
+const colorsDiv = document.querySelector('.colors')
+
+function generateColors(){
+    colorsDiv.innerHTML = ''
+
+    for(let i = 0; i < 5; i++){
+        const color = generateRandomColor()
+        const colorDiv = document.createElement('div')
+        colorDiv.style.backgroundColor = color
+        const colorName = document.createElement('p')
+        colorName.textContent = color
+        colorName.style.color = color
+        colorDiv.appendChild(colorName)
+        colorsDiv.appendChild(colorDiv)
+    }
+}
+
+function generateRandomColor(){
+    const letters = '0123456789ABCDEF'
+    let color = '#'
+
+    for(let i = 0; i < 6; i++){
+        color += letters[Math.floor(Math.random() * 16)]
+    }
+    return color
+}
+
+generateButton.addEventListener('click', generateColors)
